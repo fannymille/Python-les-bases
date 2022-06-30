58913b2e8b47dbb9b2f2deafc8bc2036b126b678
fanny <fannyvolc@gmail.com>
2022-06-30 23:01:29 +0200

    init1

---
+++ hellow.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*# pour spécifier le codage des caractères
+print (5) # affichage de la valeur 5
+a = 5
+print (a) # affichage du contenu de a
+print ("a = ", a) # affichage d'un message et du contenu de a
+
+b = 5.50
+print ("b = ", b) # affichage d'un message, du contenu de a et de b sur la même ligne
+c = 5,50,14
+print ("c = ",c)
+texte="Mon texte"
+print (texte)
+print()
+nom = input("Quel est votre nom : ")
+print ("nom = ",nom)
+# Affichage des types
+print ()
+print ("type de a: ",type(a))
+print ("type de b: ",type(b))
+print ("type de c: ",type(c))
+print ("type de texte: ",type(texte))
+print ()
+print ("type de nom: ",type(nom))
